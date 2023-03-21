4c439609b50f4fc48e8b2cfc55053acc8fd6f5bb
Rhys <rhyswoody@gmail.com>
2023-03-20 19:11:30 -0700

    change to uppercase version of string

--- dictionaries/character_count.py
+++ dictionaries/character_count.py
@@ -1,6 +1,6 @@
 message =  'My name is Carlos Sainz and I am race car Driver for Scuderia Ferrari'
 count = {}
-for character in message:
+for character in message.upper():
     count.setdefault(character, 0)
     count[character] += 1
     
