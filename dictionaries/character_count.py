37d07f2649af6e7223bca35302ba261b557c3962
Rhys <rhyswoody@gmail.com>
2023-03-20 19:14:11 -0700

    use pprint

--- dictionaries/character_count.py
+++ dictionaries/character_count.py
@@ -1,7 +1,9 @@
+import pprint
+
 message =  'My name is Carlos Sainz and I am race car Driver for Scuderia Ferrari'
 count = {}
 for character in message.upper():
     count.setdefault(character, 0)
     count[character] += 1
     
-print(count)
+pprint.pprint(count)
