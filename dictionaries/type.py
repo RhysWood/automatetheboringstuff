80e71ad109c40194c59f252e3634e480b45a7af9
Rhys <rhyswoody@gmail.com>
2023-03-26 13:22:33 -0700

    type function

---
+++ dictionaries/type.py
@@ -0,0 +1,11 @@
+#the type function is the same as JS's typeof
+print(type(42))
+#<class 'int'>
+print(type('hello'))
+#<class 'str'>
+print(type(3.14))
+#<class 'float'>
+
+theBoard = {'top-L': ' ', 'top-M': ' ', 'top-R': ' ', 'mid-L': ' ', 'mid-M': ' ', 'mid-R': ' ', 'low-L': ' ', 'low-M': ' ', 'low-R': ' '}
+print(type(theBoard))
+#<class 'dict'>
