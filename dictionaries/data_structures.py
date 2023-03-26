e0c8933f3f3729e286c9344ca9a1074c4fa9dd55
Rhys <rhyswoody@gmail.com>
2023-03-26 13:14:45 -0700

    create tic tac toe board

---
+++ dictionaries/data_structures.py
@@ -0,0 +1,14 @@
+#making lists of dictionaries
+winston = {'name': 'Winston', 'age': '12', 'color': 'grey'}
+alfie = {'name': 'Alfie', 'age': '10', 'color': 'orange'}
+pooka = {'name': 'Pooka', 'age': '5', 'color': 'black'}
+allCats = []
+allCats.append(winston)
+allCats.append(alfie)
+allCats.append(pooka)
+#this is a data structure
+print(allCats)
+#[{'name': 'Winston', 'age': '12', 'color': 'grey'}, 
+# {'name': 'Alfie', 'age': '10', 'color': 'orange'}, 
+# {'name': 'Pooka', 'age': '5', 'color': 'black'}]
+
