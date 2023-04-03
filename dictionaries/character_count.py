720a6d1bbe552cc03228111db7e66b536cec5a51
Rhys <rhyswoody@gmail.com>
2023-04-03 08:44:50 -0700

    .

--- dictionaries/character_count.py
+++ dictionaries/character_count.py
@@ -7,3 +7,20 @@ for character in message.upper():
     count[character] += 1
     
 pprint.pprint(count)
+#{' ': 13,
+# 'A': 9,
+#'C': 4,
+# 'D': 3,
+# 'E': 5,
+# 'F': 2,
+# 'I': 6,
+# 'L': 1,
+# 'M': 3,
+# 'N': 3,
+# 'O': 2,
+# 'R': 10,
+# 'S': 4,
+# 'U': 1,
+# 'V': 1,
+# 'Y': 1,
+# 'Z': 1 }
